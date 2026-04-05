cb29753ec9a8a559ce82d4c48f25e51718755e4d
NgKiet <kietnq.ce191198@gmail.com>
2026-04-06 03:27:42 +0700

    Fix URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -492,20 +492,24 @@ create: (payload, imageFile) => {
 
   return apiProduct.post("/categories", payload);
 },
-update: (id, payload, imageFile) => {
+update: async (id, payload, imageFile) => {
   const formData = new FormData();
+
   formData.append(
     "data",
-    new Blob([JSON.stringify(payload)], { type: "application/json" })
+    new Blob([JSON.stringify(payload)], {
+      type: "application/json",
+    })
   );
+
   if (imageFile) {
     formData.append("image", imageFile);
   }
-  return apiProduct.post(`/categories/${id}`, formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+  for (let pair of formData.entries()) {
+    console.log(pair[0], pair[1]);
+  }
+
+  return apiProduct.post(`/categories/${id}`, formData);
 },
   delete: async (id) => {
     const res = await apiProduct.delete(`/categories/${id}`);
